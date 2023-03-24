48c927c941b326ada3c1664230122c4ba23f5bb6
Rudiney Liemes <rudiney.liemes@gmail.com>
2023-03-24 20:51:21 -0300

    deletando arquivos do projeto

---
+++ main.js
@@ -0,0 +1,12 @@
+import  CardGame  from './src/components/CardGame'
+
+
+const $root = document.querySelector("#root");
+
+const $htmlCardGame = CardGame()
+
+console.log($htmlCardGame);
+
+$root.insertAdjacentHTML('beforeend', $htmlCardGame); 
+
+
---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+//JSX pesquisar sobre, apenas para conhecimento.
+//criando componente com javascript
+function CardGame () {
+    return `
+        <article>
+            CardGame - OI
+        </article>
+    `;
+}
+
+export default CardGame;
